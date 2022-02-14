353235164e2b62c840f3fd6ac2c70b1fc9d38a52
LANNDS18 <lannds18@icloud.com>
2022-02-14 20:22:55 +0000

    re-name

--- dddqn_train.py
+++ dddqn_train.py
@@ -5,10 +5,14 @@ from DeepRL.agents.double_dqn import DoubleDQNAgent
 from config import *
 
 
-def train_dqn():
+def train_dddqn():
 
     game = GameEnv(ENV_NAME, output_shape=IMAGE_SHAPE)
-    model = build_dueling_network(game.action_space.n, learning_rate=LEARNING_RATE, input_shape=IMAGE_SHAPE)
+
+    model = build_dueling_network(game.action_space.n,
+                                  learning_rate=LEARNING_RATE,
+                                  input_shape=IMAGE_SHAPE)
+
     buffer = ExperienceReplay(size=BUFFER_SIZE, batch_size=BATCH_SIZE)
 
     agent = DoubleDQNAgent(env=game,
@@ -16,11 +20,11 @@ def train_dqn():
                            buffer=buffer,
                            gamma=GAMMA,
                            model_path=DDDQN_PATH,
-                           log_history=True
-                           )
+                           log_history=True)
+
     agent.fill_buffer()
-    agent.learn(max_steps=10000)
+    agent.learn(max_steps=int(1e9))
 
 
 if __name__ == '__main__':
-    train_dqn()
+    train_dddqn()
