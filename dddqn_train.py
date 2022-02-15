a68f260209915913f5db11e9e09110e496cf8373
LANNDS18 <lannds18@icloud.com>
2022-02-15 16:36:06 +0000

    Optimize training

--- dddqn_train.py
+++ dddqn_train.py
@@ -6,27 +6,31 @@ from config import *
 
 
 def train_dddqn():
-
-    game = GameEnv(ENV_NAME, output_shape=IMAGE_SHAPE)
+    game = GameEnv(ENV_NAME,
+                   output_shape=IMAGE_SHAPE,
+                   frame_stack=FRAME_STACK)
 
     model = build_dueling_network(game.action_space.n,
                                   learning_rate=LEARNING_RATE,
-                                  input_shape=IMAGE_SHAPE)
+                                  input_shape=IMAGE_SHAPE,
+                                  frame_stack=FRAME_STACK)
 
     buffer = ExperienceReplay(size=BUFFER_SIZE, batch_size=BATCH_SIZE)
 
-    agent = DoubleDQNAgent(env=game,
-                           model=model,
-                           buffer=buffer,
-                           gamma=GAMMA,
-                           epsilon_start=EPSILON_START,
-                           epsilon_end=EPSILON_END,
-                           epsilon_decay_steps=EPSILON_DECAY_STEPS,
-                           model_path=DDDQN_PATH,
-                           log_history=True,)
+    agent = DoubleDQNAgent(
+        env=game,
+        model=model,
+        buffer=buffer,
+        gamma=GAMMA,
+        epsilon_start=EPSILON_START,
+        epsilon_end=EPSILON_END,
+        epsilon_decay_steps=EPSILON_DECAY_STEPS,
+        model_path=DDDQN_PATH,
+        log_history=True,
+    )
 
     agent.fill_buffer()
-    agent.learn(max_steps=TRAINING_STEP)
+    agent.learn(max_steps=TRAINING_STEP, target_reward=TARGET_MEAN_REWARD)
 
 
 if __name__ == '__main__':
